58faa779d0988ed5b7e2f422d621ec961e5f454d
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2023-12-20 11:41:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	return sampleObject.hasOwnProperty(key);
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// const key = prompt("Enter Key.");
+// alert(hasKey(key));
